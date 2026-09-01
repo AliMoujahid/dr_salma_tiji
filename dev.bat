cc98d20e6762d8a3384683d37b9b86a391d0516d
ali_moujahid <83188815+AliMoujahid@users.noreply.github.com>
2026-09-01 11:52:12 +0100

    dev bat

---
+++ dev.bat
@@ -0,0 +1,8 @@
+@echo off
+title Cabinet Dr. Salma Tijini - Development Server (Frontend + Backend)
+cls
+echo ================================================================
+echo    LANCEMENT SIMULTANE DES SERVEURS FRONTEND ET BACKEND
+echo ================================================================
+echo.
+npm run dev
